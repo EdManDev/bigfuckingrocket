64ca6cf528334b1748d0254c9050e2a144c48472
EdManDev <ed.manigat@gmail.com>
2019-04-14 20:08:02 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Index(props) {
+  return (
+    <div>
+      <div className="text">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+const text = {
+  date: new Date()
+};
+
+ReactDOM.render(<Index date={text.date} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
